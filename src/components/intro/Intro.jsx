63a0dab1130105d9be48387a457127d6fce7e94f
Dasha <dasha.kotova.mail@gmail.com>
2021-06-16 19:18:50 -0400

    add intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import "./intro.scss";
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      o hai
+      <div className="left">
+        <img src="assets/catprofile.jpeg" alt="" />
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Dasha</h1>
+          <h3>Freelance Developer</h3>
+          <ArrowDownwardIcon className="arrow" />
+        </div>
+        
+      </div>
     </div>
   );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,17 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      o hai
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img>
+        </div>
+      </div>
     </div>
   );
 }
